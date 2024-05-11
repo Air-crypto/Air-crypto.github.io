715b850d26417599edea887d4ecaa076caf9eca1
Arihan Yadav <57842655+Air-crypto@users.noreply.github.com>
2024-05-11 15:11:08 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const profileImage = document.getElementById('profilePicture');
+    profileImage.addEventListener('click', function() {
+        alert('Arihan Yadav: Pursuing a Bachelor of Science in Computer Engineering at University of Wisconsin-Madison');
+    });
+});

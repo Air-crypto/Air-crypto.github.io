aeaad275a33be76873c93fb029f17e3ef02795b6
Arihan Yadav <57842655+Air-crypto@users.noreply.github.com>
2024-05-11 16:42:47 -0700

    reverted timeline

--- scripts.js
+++ scripts.js
@@ -11,14 +11,3 @@ function closeModal() {
     var modal = document.getElementById('lightbox-modal');
     modal.style.display = 'none';
 }
-
-// Function to toggle the display of timeline event details
-function toggleTimelineDetail(eventId) {
-    var content = document.getElementById(eventId);
-    if (content.style.display === 'block') {
-        content.style.display = 'none';
-    } else {
-        content.style.display = 'block';
-    }
-}
-
